006570bd4fee819342ec0a486d865dc9192dbd3b
Linus Sellberg <linus.sellberg@nj.se>
2023-12-04 07:42:24 +0100

    day4, a bit cleaner

--- day4.cr
+++ day4.cr
@@ -1,15 +1,13 @@
 input = File.read("input.day4").lines
-cards = input.map &.split(':', 2)[1].split('|').map &.split
-matches = cards.map { |card| (card[0] & card[1]).size }
+card_numbers = input.map &.partition(':')[2].split('|').map &.split
+matches = card_numbers.map { |numbers| (numbers[0] & numbers[1]).size }
 
-sums = matches.sum { |card| card.zero? ? 0 : (2 ** (card - 1)) }
-puts "part1: %s" % sums
+puts "part1: %s" % matches.sum { |count| 1 << (count - 1) }
 
-counts = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+counts = Array.new(matches.size, 1)
 val = matches.sum do |c|
   v = counts.shift
   c.times { |i| counts[i] += v }
-  counts << 1
   v
 end
 
